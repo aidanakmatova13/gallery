a00f2dd209afad3d3dfe751e020803054e7d7141
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-05 18:20:29 +0300

    dix code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const images = document.querySelectorAll('.image')
 const main = document.querySelector('.main img')
 
+
+
 images.forEach(image =>{
     image.addEventListener('click', ()=>{
         main.src = image.src    //src - ссылка
@@ -8,3 +10,17 @@ images.forEach(image =>{
         image.classList.add('border')
     })
 })
+
+
+
+// const smallImages = document.querySelectorAll('.small-img')
+// const smallWrapper = document.querySelector('.small-wrapper')
+// const bigImg = document.querySelector('.big-img')
+//
+// smallWrapper.addEventListener('click', (e)=>{
+//     if (e.target.classList[0] === 'small-img'){
+//         bigImg.src = e.target.src
+//         smallImages.forEach(item => item.classList.remove('active'))
+//         e.target.classList.add('active')
+//     }
+// })
